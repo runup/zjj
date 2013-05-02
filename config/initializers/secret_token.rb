f3e91eacd16dde04edc10ec7cffead99fb1c9892
runup <645112281@qq.com>
2013-05-02 09:50:29 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zjj::Application.config.secret_token = 'a4adc23249d868cf4a0b7d50e3acbdc0e8c10d4e460974a75c691769370ef1999b897e605ed4c25c89d167eb27b0be46a6e82b37928e14763ad05adef7194119'
